5e17900968537b618816ba16aa2a587fd4c0b0d6
bleeeeek <mame0009@student.monash.edu>
2025-01-30 14:30:47 +0800

    app updates for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Element } from 'react-scroll';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Header from './components/Header';
 import DonationChart from './components/DonationChart';
 import EventsList from './components/EventsList';
@@ -12,6 +13,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="min-h-screen bg-white text-black">
+      <SpeedInsights />
       <Header />
       
       <main className="container mx-auto px-4 py-6 md:py-12">
